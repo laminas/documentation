29381b9a48f7217cdb00a9cfc599cf52766b9e74
Matthew Weier O'Phinney <matthew@zend.com>
2015-06-15 12:20:33 -0500

    Use CDN for default CSS
    - Moved doc-specific CSS to an S3 bucket; allows generating docs per-repo with
      no dependencies other than a path to the templates.

--- doc/templates/head.php
+++ doc/templates/head.php
@@ -3,5 +3,5 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" />
     <link rel="stylesheet" href="http://uploads.mwop.net/prism-zf.css">
-    <link rel="stylesheet" href="css/docs.css" />
+    <link rel="stylesheet" href="http://uploads.mwop.net/zf-docs.css" />
 </head>
